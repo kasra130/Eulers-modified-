f6a4d0024ee60a7c14446a75ef96b86b8b67f236
kasra130 <75147013+kasra130@users.noreply.github.com>
2021-04-10 23:56:09 +0300

    Modified Euler matlab
    Method

---
+++ Modifiedeulersmethid_likemodule.m
@@ -0,0 +1,19 @@
+dy = @(x,y)0.5*y^(2) + 0.04*x^6;
+h=0.02;
+y=1;   %%initial conition 
+x0=0;      %% start 
+xf=0.2;    %% end value 
+fprintf('\n');
+
+for x = x0 : h : xf-h 
+    xo = x+(h/2);
+    yo = y + (h/2)*(dy(x,y));
+    ya = y + h*(dy(xo,yo));
+    x = x+h;
+    y = ya
+    fprintf('%f \t %f \t',x,ya);
+end
+
+    
+    
+    
